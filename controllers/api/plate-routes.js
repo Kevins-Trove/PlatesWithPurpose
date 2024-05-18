eab4b5cdbd1f440da9aa6a4465bf64a899fdcc5f
Rubenvill0811 <rubenvill0811@gmail.com>
2024-05-18 08:55:29 -0700

    I moved the logic on give.js to plate-routes and now the endpoint is workikng.

--- controllers/api/plate-routes.js
+++ controllers/api/plate-routes.js
@@ -125,6 +125,60 @@ router.post('/order', async (req, res) => {
 
 })
 
+router.get('/give', async (req, res) => {
+
+    
+  let isAdmin = false;
+  let isReceiver = true;
+
+
+  if (req.session.logged_in){
+      const user = await User.findByPk(req.session.user_id);
+
+      const userData = user.get({plain: true})
+
+      if (userData.type == constants.ADMIN){
+          isAdmin = true;
+        } else if (userData.type == constants.RECEIEVER) {
+          isReceiver = false;
+        }
+  } else {
+      res.render('login');
+  }
+
+  try {
+      const dbOrderData = await Plate.findAll();
+
+      const activeOrders = dbOrderData.map((item) =>
+      item.get({plain: true}))
+
+      res.render('give', {
+          activeOrders, userData, logged_in: true, isAdmin, isGiver: !isReceiver
+      });
+      console.log(activeOrders);
+  } catch (err) {
+      console.error(err);
+      res.status(400).json(err);
+  }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
 
 
